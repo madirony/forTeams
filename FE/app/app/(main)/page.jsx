522d489334103e473c78b2e3c6b8f057f6f89308
노성은 <seroh00@naver.com>
2024-04-24 17:14:34 +0900

    #S10P31S204-136 [FE FEAT] Menubar 컴포넌트  생성

--- FE/app/app/(main)/page.jsx
+++ FE/app/app/(main)/page.jsx
@@ -1,9 +1,12 @@
 import styles from "styles/main.module.css";
 
+// import MenuBar from "component/menuBar";
+
 export default function Main() {
   return (
     <div className={styles.div}>
       <p>Welcome to Main Page!!</p>
+      {/* <MenuBar /> */}
     </div>
   );
 }
---
+++ FE/app/component/menuBar.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Image from "next/image";
+import styles from "styles/menuBar.module.css";
+
+export default function MenuBar() {
+  const onClick = () => {
+    //로고 클릭 시 이벤트 달기
+    console.log("로고 클릭");
+  };
+  return (
+    <div className={styles.menu}>
+      <div onClick={onClick}>
+        <Image
+          src="icon/forTeams.svg"
+          alt="Logo"
+          width={125}
+          height={90}
+        ></Image>
+      </div>
+      <div className={styles.gap}>
+        <p className={styles.text}>철강영업팀</p>
+        <p className={styles.text}>이수민님</p>
+        <p>마이페이지버튼</p>
+      </div>
+    </div>
+  );
+}
