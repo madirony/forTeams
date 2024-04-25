4196c27041797b5ef5cb869c37a9f75eb4bf2210
연정흠 <madirony52@kakao.com>
2024-04-25 10:02:01 +0900

    #S10P31S204-109  [BE FEAT] Init OpenChat Project

---
+++ BE/openchat/src/main/java/com/forteams/openchat/OpenchatApplication.java
@@ -0,0 +1,13 @@
+package com.forteams.openchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenchatApplication.class, args);
+	}
+
+}
---
+++ BE/openchat/src/test/java/com/forteams/openchat/OpenchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forteams.openchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
