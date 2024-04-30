3b39b6d2af92ab398b039769a7e5d4016ca997b5
이수민 <eelsm6@naver.com>
2024-04-30 16:21:03 +0900

    [#204] [FE FEAT] 마이페이지 구현

--- FE/app/template/mypage/mypageAllLogs.jsx
+++ FE/app/template/mypage/mypageAllLogs.jsx
@@ -1,9 +1,11 @@
 import styles from "styles/template/mypageAllLogs.module.css";
+import HistoryList from "component/historyList";
 
 export default function MypageAllLogs() {
   return (
     <div>
       <p>New Component</p>
+      <HistoryList />
     </div>
   );
 }
