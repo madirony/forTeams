ac22482f4a2792477de6546a882ac86f87dcba99
노성은 <seroh00@naver.com>
2024-05-02 16:56:56 +0900

    [#S10P31S204-198] [FE FEAT] 실시간채팅 템플릿 구현

--- FE/app/app/(main)/page.jsx
+++ FE/app/app/(main)/page.jsx
@@ -2,11 +2,17 @@ import styles from "styles/page/main.module.css";
 import MenuBar from "component/menuBar.jsx";
 
 export default function Main() {
+  // Local에서 사용자 정보를 조회해오기
+  const userName = "이수민";
+  const userDept = "철강영업팀";
+  const userId = 66;
+
   return (
     <div className={styles.root}>
-      <MenuBar />
+      <MenuBar userName={userName} userDept={userDept} userId={userId} />
       <div className={styles.content}>
-        <p>Welcome to Main Page!!</p>
+        <div className={styles.sideWrapper}>Side Wrapper</div>
+        <div className={styles.mainWrapper}>Main Wrapper</div>
       </div>
     </div>
   );
--- FE/app/component/menuBar.jsx
+++ FE/app/component/menuBar.jsx
@@ -2,26 +2,40 @@
 
 import Image from "next/image";
 import styles from "styles/component/menuBar.module.css";
+import { useRouter } from "next/navigation";
 
-export default function MenuBar() {
-  const onClick = () => {
-    //로고 클릭 시 이벤트 달기
-    console.log("로고 클릭");
+export default function MenuBar({ userName, userDept, userId }) {
+  const router = useRouter();
+
+  // 로고 클릭 이벤트
+  const logoOnClick = () => {
+    router.push("/");
   };
+
+  // 마이페이지 버튼 클릭 이벤트
+  const mypageOnClick = () => {
+    router.push("/mypage");
+  };
+
   return (
-    <div className={styles.menu}>
-      <div className={styles.imgae} onClick={onClick}>
-        <Image
-          src="icon/forTeams.svg"
-          alt="Logo"
-          width={125}
-          height={90}
-        ></Image>
+    <div className={styles.menuBar}>
+      <div className={styles.logo} onClick={logoOnClick}>
+        {/* <source media="(max-width: 360px)" src="icon/forTeams.svg" />
+        <source media="(min-width: 361px)" />
+        <Image src="icon/forTeams.svg" alt="Logo" width={125} height={90} /> */}
       </div>
-      <div className={styles.gap}>
-        <p className={styles.text}>철강영업팀</p>
-        <p className={styles.text}>이수민님</p>
-        <p>마이페이지버튼</p>
+      <div className={styles.userInfo}>
+        <p>{userDept}</p>
+        <p>{userName}님</p>
+        <div className={styles.mypageButton}>
+          <Image
+            onClick={mypageOnClick}
+            src="icon/myPageButton.svg"
+            alt="Mypage Button"
+            width={125}
+            height={90}
+          />
+        </div>
       </div>
     </div>
   );
