176c649d5ed3b652bb2831babd2eb0635f902a0f
노성은 <seroh00@naver.com>
2024-05-03 17:26:46 +0900

    [#S10P31S204-198] [FE FEAT] recoFunction 템플릿 작성

--- FE/app/app/(main)/page.jsx
+++ FE/app/app/(main)/page.jsx
@@ -1,5 +1,29 @@
+// import styles from "styles/page/main.module.css";
+// import MenuBar from "component/menuBar.jsx";
+
+// export default function Main() {
+//   // Local에서 사용자 정보를 조회해오기
+//   const userName = "이수민";
+//   const userDept = "철강영업팀";
+//   const userId = 66;
+
+//   return (
+//     <div className={styles.root}>
+//       <MenuBar userName={userName} userDept={userDept} userId={userId} />
+//       <div className={styles.content}>
+//         <div className={styles.sideWrapper}>Side Wrapper</div>
+//         <div className={styles.mainWrapper}>Main Wrapper</div>
+//       </div>
+//     </div>
+//   );
+// }
+
 import styles from "styles/page/main.module.css";
 import MenuBar from "component/menuBar.jsx";
+import ChatMain from "template/chat/chatMain";
+import RecoFunctionMain from "template/recoFunction/recoFunctionMain";
+import ChatBotMain from "template/chatBot/chatBotMain";
+import ChatBotInput from "component/chatBotInput";
 
 export default function Main() {
   // Local에서 사용자 정보를 조회해오기
@@ -11,8 +35,16 @@ export default function Main() {
     <div className={styles.root}>
       <MenuBar userName={userName} userDept={userDept} userId={userId} />
       <div className={styles.content}>
-        <div className={styles.sideWrapper}>Side Wrapper</div>
-        <div className={styles.mainWrapper}>Main Wrapper</div>
+        <div className={styles.sideWrapper}>
+          {/* Side Wrapper */}
+          {/* <ChatMain /> */}
+          <RecoFunctionMain />
+        </div>
+        <div className={styles.mainWrapper}>
+          {/* Main Wrapper */}
+          <ChatBotMain />
+          <ChatBotInput />
+        </div>
       </div>
     </div>
   );
