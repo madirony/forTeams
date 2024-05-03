9088a158d071318be08e3998e6a8962b86789bab
이수민 <eelsm6@naver.com>
2024-05-03 17:29:06 +0900

    [#204] [FE FEAT] 반응형 페이지 구현

--- FE/app/app/(main)/page.jsx
+++ FE/app/app/(main)/page.jsx
@@ -1,5 +1,8 @@
 import styles from "styles/page/main.module.css";
 import MenuBar from "component/menuBar.jsx";
+import MainSwiper from "template/main/mainSwiper";
+import ChatMain from "template/chat/chatMain";
+import ChatBotMain from "template/chatBot/chatBotMain";
 
 export default function Main() {
   // Local에서 사용자 정보를 조회해오기
@@ -10,9 +13,16 @@ export default function Main() {
   return (
     <div className={styles.root}>
       <MenuBar userName={userName} userDept={userDept} userId={userId} />
-      <div className={styles.content}>
-        <div className={styles.sideWrapper}>Side Wrapper</div>
-        <div className={styles.mainWrapper}>Main Wrapper</div>
+      <div className={styles.largePage}>
+        <div className={styles.sideWrapper}>
+          <ChatMain />
+        </div>
+        <div className={styles.mainWrapper}>
+          <ChatBotMain />
+        </div>
+      </div>
+      <div className={styles.swiperPage}>
+        <MainSwiper />
       </div>
     </div>
   );
