977dec9d4f155c7b79772203311f554f55122edd
김봉균 <qhdrnak@hanyang.ac.kr>
2024-05-07 10:37:11 +0900

    [#S10P31S204-187] [BE-TEST] Modify Folder api

--- BE/chatbot/src/main/java/com/forteams/chatbot/folder/dto/FolderResponseDto.java
+++ BE/chatbot/src/main/java/com/forteams/chatbot/folder/dto/FolderResponseDto.java
@@ -1,4 +1,15 @@
 package com.forteams.chatbot.folder.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@Getter
+@Setter
 public class FolderResponseDto {
+    private Long id;
+    private String name;
 }
