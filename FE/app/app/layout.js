e8657ff88ed7436eac75018436679b619f707479
노성은 <seroh00@naver.com>
2024-05-08 18:33:35 +0900

    [FE FEAT] save.js api, dropdownInput 수정

--- FE/app/app/layout.js
+++ FE/app/app/layout.js
@@ -1,5 +1,6 @@
 import "styles/globals.css";
 import LocalStorage from "util/localStorage";
+import Head from "next/head";
 import RecoilRootProvider from "util/recoilRootProvider";
 import LoginMain from "./login/page";
 
@@ -14,13 +15,26 @@ const isLogined = true;
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      {/* <head>
         <link rel="manifest" href="manifest.json" />
         <script type="module">
           import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate'; const el
           = document.createElement('pwa-update'); document.body.appendChild(el);
         </script>
-      </head>
+      </head> */}
+      <Head>
+        <link rel="manifest" href="manifest.json" />
+        <script
+          type="module"
+          dangerouslySetInnerHTML={{
+            __html: `
+          import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+          const el = document.createElement('pwa-update');
+          document.body.appendChild(el);
+        `,
+          }}
+        />
+      </Head>
       <RecoilRootProvider>
         {isLogined ? (
           <body>{children}</body>
