96eeb59aa2e18723a67a293ccc609cea5e28ec10
연정흠 <madirony52@kakao.com>
2024-05-13 18:55:26 +0900

    #S10P31S204-287  [BE TEST] 배포 TimeZone 테스트

--- BE/openchat/src/main/java/com/forteams/openchat/chat/service/OpenChatService.java
+++ BE/openchat/src/main/java/com/forteams/openchat/chat/service/OpenChatService.java
@@ -37,14 +37,8 @@ public class OpenChatService {
 
         log.info(String.valueOf(startOfDay) + String.valueOf(endOfDay));
 
-        // 서울 시간대에서 UTC로 변환
-        LocalDateTime startOfTodayInUTC = startOfDay.atZone(seoulZoneId).withZoneSameInstant(ZoneOffset.UTC).toLocalDateTime();
-        LocalDateTime endOfTodayInUTC = endOfDay.atZone(seoulZoneId).withZoneSameInstant(ZoneOffset.UTC).toLocalDateTime();
-
-        log.info(String.valueOf(startOfTodayInUTC) + String.valueOf(endOfTodayInUTC));
-
         // 데이터베이스 조회
-        List<OpenChat> chatsFromDb = openChatRepository.findByCreatedAtBetween(String.valueOf(startOfTodayInUTC), String.valueOf(endOfTodayInUTC));
+        List<OpenChat> chatsFromDb = openChatRepository.findByCreatedAtBetween(String.valueOf(startOfDay), String.valueOf(endOfDay));
         List<OpenChatDto> result = chatsFromDb.stream().map(this::convertToDto).collect(Collectors.toList());
         result.addAll(getChatsFromRedis());
         return result;
