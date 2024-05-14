97d40d3747e1d8ab62ce7b136594fa6809b65263
이수민 <eelsm6@naver.com>
2024-05-14 16:10:04 +0900

    [FE FEAT] 쿠키 받아오기 작업중

--- FE/app/app/layout.js
+++ FE/app/app/layout.js
@@ -3,6 +3,7 @@ import LocalStorage from "util/localStorage";
 import Head from "next/head";
 import RecoilRootProvider from "util/recoilRootProvider";
 import LoginMain from "./login/page";
+import { getCookies, GetIsLogined } from "util/getToken";
 
 export const metadata = {
   title: "forTeams",
@@ -11,7 +12,15 @@ export const metadata = {
 
 // ★★★ 여기서 로그인 여부 확인해 분기 처리하기
 // local storage에서 사용자 토큰 확인하기
-const isLogined = true;
+// const isLogined = true;
+const isLogined = GetIsLogined;
+
+// getCookies();
+// if (getCookies.sub) {
+//   let isLogined = true;
+// } else {
+//   isLogined = false;
+// }
 
 export default function RootLayout({ children }) {
   return (
--- FE/app/public/util/getToken.jsx
+++ FE/app/public/util/getToken.jsx
@@ -1,8 +1,12 @@
+"use client";
+
 // SSR 쿠키에서 토큰을 읽어오는 함수
 // const getCookies = () => {
 //   const cookieStore = cookies().getAll();
 //   console.log("모든 쿠키 :", cookieStore);
 // };
+
+import { useState } from "react";
 
 // CSR 쿠키에서 토큰을 읽어오는 함수
 const getCookies = () => {
@@ -17,17 +21,28 @@ const getCookies = () => {
 };
 
 // jwt 라이브러리로 읽어온 토큰을 파싱하는 함수
-const decodeToken = () => {
-  try {
-    const decodedToken = jwtDecode(accessToken);
-    const userID = decodedToken.sub;
-    const userName = decodedToken.nickname;
-    const userDept = decodedToken.dept;
-    return { userID, userName, userDept };
-  } catch (error) {
-    console.log("토큰 가져오기 실패 :", error);
-    return null;
+// const decodeToken = () => {
+//   try {
+//     const decodedToken = jwtDecode(accessToken);
+//     const userID = decodedToken.sub;
+//     const userName = decodedToken.nickname;
+//     const userDept = decodedToken.dept;
+//     return { userID, userName, userDept };
+//   } catch (error) {
+//     console.log("토큰 가져오기 실패 :", error);
+//     return null;
+//   }
+// };
+
+const GetIsLogined = () => {
+  console.log("get is logined 들어오긴 함 :", isLogined);
+  const [isLogined, setIsLogined] = useState(false);
+
+  const cookieObj = getCookies();
+  {
+    cookieObj.sub ? setIsLogined(true) : null;
   }
+  return isLogined;
 };
 
-export { getCookies };
+export { getCookies, GetIsLogined };
