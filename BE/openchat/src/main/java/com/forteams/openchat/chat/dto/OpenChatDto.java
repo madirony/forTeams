81b6dedbade4c087848287e4cdf04fcfc42f50c4
연정흠 <madirony52@kakao.com>
2024-05-16 19:44:55 +0900

    #S10P31S204-272  [BE FEAT] OpenChat 닉네임 추가

--- BE/openchat/src/main/java/com/forteams/openchat/chat/dto/OpenChatDto.java
+++ BE/openchat/src/main/java/com/forteams/openchat/chat/dto/OpenChatDto.java
@@ -10,10 +10,9 @@ public class OpenChatDto extends BaseEntity {
     private String senderUUID;
     private String message;
     private String messageUUID;
-
+    private String nickname;  // 닉네임 필드 추가
     private String replyMsgUUID;
     private String replyTo;
-
     private String removeCheck;
 
     @Override
--- BE/openchat/src/main/java/com/forteams/openchat/chat/entity/OpenChat.java
+++ BE/openchat/src/main/java/com/forteams/openchat/chat/entity/OpenChat.java
@@ -8,7 +8,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.time.LocalDateTime;
 
 @Document
 @Getter
@@ -21,16 +20,18 @@ public class OpenChat implements Serializable {
     private String senderUUID;
     private String message;
     private String messageUUID;
+    private String nickname;  // 닉네임 필드 추가
     private String replyMsgUUID;
     private String replyTo;
     private String removeCheck;
     private String createdAt;
     private String updatedAt;
 
-    public OpenChat(String senderUUID, String message, String messageUUID, String replyMsgUUID, String replyTo, String removeCheck, String createdAt, String updatedAt) {
+    public OpenChat(String senderUUID, String message, String messageUUID, String nickname, String replyMsgUUID, String replyTo, String removeCheck, String createdAt, String updatedAt) {
         this.senderUUID = senderUUID;
         this.message = message;
         this.messageUUID = messageUUID;
+        this.nickname = nickname;  // 닉네임 필드 초기화
         this.replyMsgUUID = replyMsgUUID;
         this.replyTo = replyTo;
         this.removeCheck = removeCheck;
