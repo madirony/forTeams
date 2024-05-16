86d1acaad0458c1c87584161155039972157263e
이수민 <eelsm6@naver.com>
2024-05-16 21:11:56 +0900

    [FE FEAT] 0516 2111

--- FE/app/template/mypage/mypageAllLogs.jsx
+++ FE/app/template/mypage/mypageAllLogs.jsx
@@ -40,7 +40,7 @@ export default function MypageAllLogs({ openModalShare, openModalSave }) {
           openModalSave={openModalSave}
         />
       ) : (
-        <>
+        <div style={{ width: "100%", height: "100%" }}>
           {datas ? (
             datas.map((data, idx) => {
               return (
@@ -56,7 +56,7 @@ export default function MypageAllLogs({ openModalShare, openModalSave }) {
               <p>챗봇에게 새로운 질문을 해 보세요!</p>
             </div>
           )}
-        </>
+        </div>
       )}
     </div>
   );
