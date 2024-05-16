ce00984fba5d51f9252278454e98926613770b19
이수민 <eelsm6@naver.com>
2024-05-16 13:51:55 +0900

    [FE FEAT] MS 로그인 구현중 1351

--- FE/app/apis/login.js
+++ FE/app/apis/login.js
@@ -1,12 +1,13 @@
-// import axios from "util/baseAPI";
-import axios from "axios";
+import axios from "util/baseAPI";
+// import axios from "axios";
 
 // 로그인 추가 정보 입력
 const addLoginInfo = async (userDept) => {
   try {
     const response = await axios({
       method: "post",
-      url: `http://localhost:8443/api/v1/auth/info?dept=${userDept}`,
+      url: `/api/v1/auth/info?dept=${userDept}`,
+      // url: `http://localhost:8443/api/v1/auth/info?dept=${userDept}`,
     });
     console.log("로그인 추가 정보 입력 성공:", response.data);
     return response.data;
@@ -20,7 +21,7 @@ const logout = async () => {
   try {
     const response = await axios({
       method: "post",
-      url: `api/v1/auth/logout`,
+      url: `/api/v1/auth/logout`,
     });
     console.log("로그아웃 요청 성공:", response.data);
     return response.data;
