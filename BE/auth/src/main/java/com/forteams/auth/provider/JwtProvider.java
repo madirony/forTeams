fef512ad517e0779f9baf4932ecbc320ae435558
김봉균 <qhdrnak@hanyang.ac.kr>
2024-05-16 12:56:54 +0900

    [#S10P31S204-323] [BE-TEST] change key

--- BE/auth/src/main/java/com/forteams/auth/provider/JwtProvider.java
+++ BE/auth/src/main/java/com/forteams/auth/provider/JwtProvider.java
@@ -45,6 +45,7 @@ public class JwtProvider {
                 .setExpiration(new Date(System.currentTimeMillis() + validity))
                 .claim("dept", user.getUserDept())
                 .claim("nickname", user.getUserNickname())
+
                 .signWith(key)
                 .compact();
     }
