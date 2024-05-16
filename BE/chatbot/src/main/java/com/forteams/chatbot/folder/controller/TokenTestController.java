c7e1ea8ebeee2fc6aaf61f2324a8384548bc7d1b
김봉균 <qhdrnak@hanyang.ac.kr>
2024-05-16 10:00:23 +0900

    [#S10P31S204-326] [BE-FIX] cors test

---
+++ BE/chatbot/src/main/java/com/forteams/chatbot/folder/controller/TokenTestController.java
@@ -0,0 +1,32 @@
+package com.forteams.chatbot.folder.controller;
+
+import com.forteams.chatbot.folder.dto.FolderResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping( "/api/v1/token-test")
+@AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000, https://forteams.co.kr")
+public class TokenTestController {
+
+    @GetMapping
+    public ResponseEntity<String> getMsUuid(@RequestHeader("msUuid") String msUuid){
+        try{
+            log.info("***************************\tgetMsUuid 도착\t***************************");
+
+
+            return ResponseEntity.ok(msUuid);
+        }catch (Exception e){
+            log.info(">>>>>"+e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+    }
+}
