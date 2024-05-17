16f7fdb28cba13cf8cf1c445890ce29b44afdc75
이수민 <eelsm6@naver.com>
2024-05-17 13:40:32 +0900

    [FE FEAT] 0517 1340

--- FE/app/app/login/info/page.jsx
+++ FE/app/app/login/info/page.jsx
@@ -1,9 +1,5 @@
-//
-
 "use client";
 
-// import { cookies } from "next/headers";
-import jwtDecode from "jwt-decode";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import styles from "styles/page/loginInfo.module.css";
@@ -15,45 +11,33 @@ import { addLoginInfo } from "../../../apis/login";
 
 export default function LoginInfo() {
   const router = useRouter();
-  // 토큰 및 사용자 정보 읽어오기 =============================================
-  // 쿠키에서 토큰을 읽어오는 함수
-
-  // 사용자 이름, 부서 정보 저장하기
-  // const [name, setName] = useState("");
-  // const [department, setDepartment] = useState({});
 
   // 드롭다운 정의 ===========================================================
   const [selectedOption, setSelectedOption] = useState({});
-  // console.log("login/info 부서 선택 바뀜: ", selectedOption.name);
+  // console.log("[LoginInfo] 부서 선택 바뀜: ", selectedOption.name);
 
   // 버튼 핸들러 =============================================================
   const handlePurpleButtonClick = () => {
     const userDept = selectedOption.name;
-    console.log("1. 유저가 선택한 부서 이름 알기", userDept);
+    console.log("[LoginInfo] 1. 유저가 선택한 부서 이름 알기", userDept);
     // 로그인 추가 정보 입력 API 요청 보내기
     addLoginInfo(userDept).then((response) => {
-      console.log("2. 부서 이름 보내서 토큰 생성", response);
+      console.log("[LoginInfo] 2. 부서 이름 보내서 토큰 생성", response);
 
       // 메인 페이지로 리다이렉트
-      console.log("3. 메인 페이지로 리다이렉트");
+      console.log("[LoginInfo] 3. 메인 페이지로 리다이렉트");
       router.push("/");
-
-      // 완료 버튼 클릭!
-      // 회원가입 완료 msUuid: c37afcd8-c1f4-42b7-9be8-ec2bf6d24622,
-      // dept: undefined
     });
   };
 
   const handleGrayButtonClick = () => {
     router.back();
-    console.log("취소 버튼 클릭!");
-    // 여기에 수행할 작업 추가
-    // 로그인 페이지(이전 페이지)로 돌아가기
+    // console.log("[LoginInfo] 취소 버튼 클릭!");
   };
 
   return (
     <div className={styles.container}>
-      <p className={styles.modalTitle}>사용자 정보 1115</p>
+      <p className={styles.modalTitle}>사용자 정보</p>
       <div className={styles.inputContainer}>
         <p className={styles.inputTitle}>닉네임</p>
         <input
