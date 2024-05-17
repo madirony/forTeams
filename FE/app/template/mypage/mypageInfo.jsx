36a3adab403e80e2a85c043cd81856d3b37b303b
이수민 <eelsm6@naver.com>
2024-05-17 17:28:14 +0900

    [FE FEAT] 0517 1728

--- FE/app/template/mypage/mypageInfo.jsx
+++ FE/app/template/mypage/mypageInfo.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Input from "component/input";
 import GradientButton from "component/gradientButton";
 import LoginDropdownInput from "component/loginDropdownInput";
+import updateDept from "apis/login";
 
 export default function MypageInfo() {
   // ★Local에서 사용자 정보를 조회 =================================
@@ -14,18 +15,29 @@ export default function MypageInfo() {
   // 드롭다운 정의 ===========================================================
   const [selectedOption, setSelectedOption] = useState({});
 
+  // 완료 버튼 눌렀을 때 실행될 함수
+  const onClickButton = () => {
+    updateDept(userId, selectedOption.name).then((response) => {
+      console.log("수정 요청 성공");
+    });
+  };
+
   return (
     <div className={styles.wrapper}>
       <Input title={"닉네임"} message={userNickname} readonly />
       <Input title={"부서"} message={userDept} readonly />
+      수정할 부서를 선택하고 완료 버튼을 눌러주세요
       <LoginDropdownInput
         selectedOption={selectedOption}
         setSelectedOption={setSelectedOption}
       />
-      선택된 부서 {selectedOption.name}
       {/* 수정 버튼은 일단 삭제 */}
       <div className={styles.button}>
-        <GradientButton mode={"ONE_BUTTON"} purpleButtonText={"완료"} />
+        <GradientButton
+          mode={"ONE_BUTTON"}
+          purpleButtonText={"완료"}
+          onPurpleButtonClick={onClickButton}
+        />
       </div>
     </div>
   );
