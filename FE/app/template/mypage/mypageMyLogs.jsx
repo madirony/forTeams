aed78680c5b3fb0ea7b51742a40aa23a90c81ca8
노성은 <seroh00@naver.com>
2024-05-17 16:11:36 +0900

    [FE FEAT] myPageMyLogs.jsx 수정 중

--- FE/app/template/mypage/mypageMyLogs.jsx
+++ FE/app/template/mypage/mypageMyLogs.jsx
@@ -43,10 +43,9 @@ export default function MypageMyLogs({ openModalShare, openModalSave }) {
 
   useEffect(() => {
     if (selectedFolder !== null) {
-      // console.log("??????", selectedFolder);
       getMyChatbotList(selectedFolder).then((response) => {
         console.log("폴더 저장 마이 로그 리스트 출력!", response);
-        // setDatas(response);
+        setDatas(response);
       });
     }
   }, [selectedFolder]);
