16831511432bccfb15a483f9f6b6b3c22cabc69c
이수민 <eelsm6@naver.com>
2024-05-17 17:47:40 +0900

    [FE FEAT] 0517 1747

--- FE/app/template/mypage/mypageInfo.jsx
+++ FE/app/template/mypage/mypageInfo.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import Input from "component/input";
 import GradientButton from "component/gradientButton";
 import LoginDropdownInput from "component/loginDropdownInput";
-import updateDept from "apis/login";
+import { updateDept } from "apis/login";
 
 export default function MypageInfo() {
   // ★Local에서 사용자 정보를 조회 =================================
@@ -37,12 +37,12 @@ export default function MypageInfo() {
     <div className={styles.wrapper}>
       <Input title={"닉네임"} message={userNickname} readonly />
       <Input title={"부서"} message={userDept} readonly />
+      {/* 수정 버튼은 일단 삭제 */}
       수정할 부서를 선택하고 완료 버튼을 눌러주세요 {selectedOption.name}
       <LoginDropdownInput
         selectedOption={selectedOption}
         setSelectedOption={setSelectedOption}
       />
-      {/* 수정 버튼은 일단 삭제 */}
       <div className={styles.button}>
         <GradientButton
           mode={"ONE_BUTTON"}
